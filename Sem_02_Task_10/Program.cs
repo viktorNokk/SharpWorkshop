dfa84f95f648778660cc2db2714b80ed2ecda506
ViktorK <nokksaxs@gmail.com>
2022-07-19 21:18:25 +0300

    add new task

--- Sem_02_Task_10/Program.cs
+++ Sem_02_Task_10/Program.cs
@@ -15,19 +15,3 @@ Console.WriteLine(str[2]);
 
 
 
-
-// Console.Write("Введите число: ");
-// int number = Convert.ToInt32(Console.ReadLine());
-
-// while (number >= 100 && number < 1000)
-// {
-//     number = number/10;
-//     if (number<100)
-//     {
-//         Console.WriteLine("Вторая (с начала) цифра : {0}", number.ToString().ToCharArray()[1]);
-//     }
-//     else
-//     {
-//         Console.WriteLine("Число не трехзначное: ");
-//     }
-// }
