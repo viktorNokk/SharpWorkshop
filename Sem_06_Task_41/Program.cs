fb915d764dd9ddca2d937048dd3afa79eb19c32f
Kotov Viktor <V.Kotov@norsi-trans.lcl>
2022-08-10 16:59:14 +0300

    added new tasks 41,43

---
+++ Sem_06_Task_41/Program.cs
@@ -0,0 +1,54 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+* 0, 7, 8, -2, -2 -> 2
+* 1, -7, 567, 89, 223-> 3
+*/
+
+int[] CreateArray(int size)
+{
+    int[] newArray = new int[size];
+
+    Console.WriteLine("Creating array here:");
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i + 1} element: " ); // чтобы начать с 1 а не с 0
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+    return newArray;   
+}
+
+int CountPositiveNum(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) count++;
+    }
+
+    return count;
+}
+
+void ShowArray(int[] array)
+
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array2 = CreateArray(size);
+ShowArray(array2);
+
+int countArr = CountPositiveNum(array2);
+
+Console.WriteLine("Count of pissitive numbers in array  is " + countArr);
+
+
+
+
+
---
+++ Sem_06_Task_43/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+* y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+* b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+*/
+
+double CrossPoint(double b1, double k1, double b2, double k2)
+{
+    double x = -(b1 - b2) / (k1 - k2);
+    double y = k2 * x + b2;
+    return x;
+    return y;
+}
+
+Console.Write("Input b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(CrossPoint(b1, k1, b2, k2));
+
+//double res = CrossPoint(b1, k1, b2, k2);
+
+//Console.WriteLine(CrossPoint(b1, k1, b2, k2));
+
+
+
+/* вычесть из 1 2е
+* y = k1 * x + b1
+* y = k2 * x + b2
+* y - y = k1 * x + b1 - k2 * x - b2
+* 0 = x(k1 - k2) + b1 - b2 
+* x(k1 - k2) = -(b1 - b2)
+* x = -(b1 - b2) / (k1 - k2)
+*/
+
+//double x = -(b1 - b2) / (k1 - k2);
+//double y = k2 * x + b2;
+
+//Console.WriteLine($"({x}; {y})");
+
+
